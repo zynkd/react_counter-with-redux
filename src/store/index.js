48139371cc70d77e757af318df4e99b922fa0199
zynkd <coding707@mailbox.org>
2023-08-25 13:44:55 +0200

    Created store and wrapped its Provider around App

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1,
+      };
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = redux.createStore(counterReducer);
+
+export default store;
