4e99174c9fada3d3efd171a488a892ab7600fd68
zynkd <coding707@mailbox.org>
2023-08-27 15:19:08 +0200

    Created separate files for each slice

---
+++ src/store/auth-slice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authSliceActions = authSlice.actions;
+export const authSliceReducer = authSlice.reducer;
