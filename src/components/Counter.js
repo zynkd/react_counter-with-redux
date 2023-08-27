af7b034a416eb27581580b65d8ba91db7f84fbfc
zynkd <coding707@mailbox.org>
2023-08-27 13:50:08 +0200

    Rewriting Counter component for Redux Toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
+import {counterActions} from '../store/index';
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
@@ -7,19 +8,23 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: 'INCREMENT' });
+    // dispatch({ type: 'INCREMENT' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = (amount) => {
-    dispatch({ type: 'INCREASE', amount });
+    // dispatch({ type: 'INCREASE', amount });
+    dispatch(counterActions.increase(10)); // passing `payload` prop. by default
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'DECREMENT' });
+    // dispatch({ type: 'DECREMENT' });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'TOGGLE_COUNTER' });
+    // dispatch({ type: 'TOGGLE_COUNTER' });
+    dispatch(counterActions.toggle());
   };
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -18,7 +18,7 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter += action.amount;
+      state.counter += action.payload;
     },
     toggle(state) {
       state.showCounter = !state.showCounter;
