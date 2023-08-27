b4794ae2be6a696a4413db5284ff4d2c144e4682
zynkd <coding707@mailbox.org>
2023-08-27 16:26:36 +0200

    Added REGEX validation for email; added dummy password validation

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,14 +1,26 @@
 import { useDispatch } from 'react-redux';
 import classes from './Auth.module.css';
 import { authSliceActions } from '../store/auth-slice';
+import { useRef } from 'react';
 
 const Auth = () => {
   const dispatch = useDispatch();
+  const inputEmailRef = useRef();
+  const inputPwdRef = useRef();
+
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(authSliceActions.login());
+    if (
+      emailRegex.test(inputEmailRef.current.value) &&
+      inputPwdRef.current.value.length >= 10
+    ) {
+      dispatch(authSliceActions.login());
+    } else {
+      inputPwdRef.current.value = '';
+    }
   };
 
   return (
@@ -17,11 +29,11 @@ const Auth = () => {
         <form onSubmit={handleFormSubmit}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <input type='email' id='email' ref={inputEmailRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <input type='password' id='password' ref={inputPwdRef} />
           </div>
           <button>Login</button>
         </form>
